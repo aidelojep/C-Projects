b552b58a2f93a9e843af35d436fb3968764e9296
aidelojep <aidelojepeter123@gmail.com>
2021-11-18 08:50:48 +0100

    Exceptions in c#

--- MFirstApplication/Program.cs
+++ MFirstApplication/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MFirstApplication
+namespace MyFirstApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+          
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+
+            int b = 1;
+
+            //Explicit casting
+            byte c = (byte) b;
+            Console.WriteLine();
+
+            try
+            {
+                String name = "12345";
+                byte number = Convert.ToByte(name);
+                Console.WriteLine(number);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+   
+            Console.WriteLine(c);
+           
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+
         }
     }
 }
